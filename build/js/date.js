b080e722ed0fc9d87dc0bb25d21f0aa90c264f16
gerard kelleher <gerardkelleher@hotmail.com>
2018-07-20 12:30:38 +0100

    final final

---
+++ build/js/date.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var hourNow=today.gethours();
+var greetings;
+// display greetings
+if (hoursNow>18){
+  greetings='good evening'
+}else if (hoursNow>12 {
+  greetings='good afternoon'
+}else{
+  greetings='welcome'
+}
+document.write(greeings);
