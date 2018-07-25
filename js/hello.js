92ec0d56a8f82240f1816567ee08fe7e768b35be
gerard kelleher <gerardkelleher@hotmail.com>
2018-07-25 16:39:26 +0100

    final wed

--- js/hello.js
+++ js/hello.js
@@ -1,14 +1,11 @@
-var  $  =  require(‘jquery’);
+var  $  =  require("jquery");
 $(function()  {
-  alert(‘it  works!’);
+  alert("it  works!");
 });
 $(function(){
-  $('h1').show.slidetoggle();
-  var $li=$('li');
+  $("h1").show.slidetoggle();
+  var $li=$("li");
   $.show().each(function(index){
-  $(this).delay(700*index).fadein(700);
-));
-$li.on('click',function() {
-$(this).fadein(700);
-));
-));
+	  $(this).delay(700*index).fadein(700);
+	});
+})();
