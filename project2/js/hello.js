a31293325c062f65186ecf47f48669046a82153a
gerard kelleher <gerardkelleher@hotmail.com>
2018-07-26 17:51:33 +0100

    final thursday

---
+++ project2/js/hello.js
@@ -0,0 +1,14 @@
+var  $  =  require(‘jquery’);
+$(function()  {
+  alert(‘it  works!’);
+});
+$(function(){
+  $('h1').show.slidetoggle();
+  var $li=$('li');
+  $.show().each(function(index){
+  $(this).delay(700*index).fadein(700);
+));
+$li.on('click',function() {
+$(this).fadein(700);
+));
+));
